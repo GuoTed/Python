216ba9c550858a5dd761f9b83d4e2116597cc036
tedguo <tednogamenolife@gmail.com>
2023-11-20 21:11:59 +0800

    Regex practice - findall

--- Regex/regex.py
+++ Regex/regex.py
@@ -3,14 +3,17 @@ import re
 # practice the regex
 sample_1 = 'ApplebannaTree[te]ctree'
 
-# form: re.search(pattern, string, flags)
+# search if has the decribed pattern
 res = re.search("t[t,r]ee", sample_1, re.I)
-print(res)
 if res:
-    print(res.group())
+    print(res.group()) # Tree
+
+# search all decribed pattern
+res = re.findall("t[t,r]ee", sample_1, re.I)
+print(res) # ['Tree', 'tree']
 
 sample_2 = '----------abc123---------'
 res = re.sub("[a-z0-9]", "|", sample_2)
-print(res)
+print(res) # ----------||||||---------
 res = re.sub("[^a-z0-9]", "*", res)
-print(res)
+print(res) # *************************
